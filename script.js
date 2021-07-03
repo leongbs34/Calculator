3612ac4745afe0ec530c39222bc9cac96ba1956c
Leong <stapler_1@hotmail.com>
2021-07-03 13:08:20 +0800

    Added functions to buttons

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let number1 = '';
+let number2 = '';
+let operator;
+
 function add(number1, number2){
     return number1 + number2;
 }
@@ -16,16 +20,16 @@ function divide(number1, number2){
 
 function operate(operator, number1, number2){
     switch(operator){
-        case '+':
+        case 'add':
             return add(number1,number2);
             break;
-        case '-':
+        case 'subtract':
             return subtract(number1,number2);
             break;
-        case '*':
+        case 'multiply':
             return multiply(number1,number2);
             break;
-        case '/':
+        case 'divide':
             return divide(number1,number2);
             break;
         default:
@@ -34,16 +38,47 @@ function operate(operator, number1, number2){
     }
 }
 
+function clearVar(){
+    number1 = '';
+    number2 = '';
+    operator = '';
+}
+
+const numbers = document.querySelectorAll('.numbers');
+const h1 = document.querySelector('h1');
+const operators = document.querySelectorAll('.operators')
+const clear = document.querySelector('#clear');
+const equals = document.querySelector('#equals');
+
 for(let i=0; i<=9; i++){
     const number = document.querySelector(`#number${i}`);
     number.setAttribute('style', `grid-area: number${i}`);
 }
 
-let number1 = '';
-let number2 = '';
-const numbers = document.querySelectorAll('.numbers');
-const h1 = document.querySelector('h1');
 numbers.forEach(number => number.addEventListener('click', function(e){
-    number1 += e.target.textContent;
-    h1.textContent = number1;
+    if(!operator){
+        number1 += e.target.textContent;
+        h1.textContent = number1;
+    }
+    else {
+        number2 += e.target.textContent;
+        h1.textContent += e.target.textContent;
+    }
 }))
+
+operators.forEach(op => op.addEventListener('click', function(e){
+    operator = e.target.id;
+    h1.textContent += e.target.textContent;
+}))
+
+clear.addEventListener('click', () => {
+    clearVar();
+    h1.textContent = 'Display';
+})
+
+equals.addEventListener('click', () => {
+    let answer = operate(operator, parseInt(number1), parseInt(number2));
+    clearVar();
+    number1 = answer;
+    h1.textContent = answer;
+})
